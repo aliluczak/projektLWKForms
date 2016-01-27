3bf6436ba6dd3f5cbd867a735242fbcfca67abbf
aliluczak <aliluczak@gmail.com>
2016-01-27 11:21:16 +0100

    ver1
    gotowa aplikacja

---
+++ projektLWKForms/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace projektLWKForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.viewer = new Emgu.CV.UI.ImageViewer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(204, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "przeglądaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // viewer
+            // 
+            this.viewer.AutoScroll = true;
+            this.viewer.ClientSize = new System.Drawing.Size(184, 161);
+            this.viewer.Image = null;
+            this.viewer.Location = new System.Drawing.Point(0, 0);
+            this.viewer.Name = "viewer";
+            this.viewer.TabStop = false;
+            this.viewer.Text = "Image";
+            this.viewer.Visible = false;
+            this.viewer.Load += new System.EventHandler(this.viewer_Load);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(86, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "rozpocznij";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 255);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SIFT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private Emgu.CV.UI.ImageViewer viewer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ projektLWKForms/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Features2D;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.XFeatures2D;
+using Emgu.CV.CvEnum;
+
+namespace projektLWKForms
+{
+    public partial class Form1 : Form
+    {
+        private Mat loadedImage { get; set; }
+        private UMat originalImageDescriptors { get; set; }
+        private VectorOfKeyPoint originalImageKeyPoints { get; set; }
+        private Capture capture;
+        private Mat grayImage;
+        private int time;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.FileName;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            loadImage(textBox1.Text);
+            grayImage = new Mat();
+            CvInvoke.CvtColor(loadedImage, grayImage, ColorConversion.Bgr2Gray);
+            originalImageDescriptors = new UMat();
+
+            originalImageKeyPoints = new VectorOfKeyPoint();
+            calculatedescriptors(loadedImage, originalImageDescriptors, originalImageKeyPoints);
+
+            videoCompute();
+        }
+
+        private void loadImage(string file)
+        {
+            loadedImage = CvInvoke.Imread(file, Emgu.CV.CvEnum.LoadImageType.AnyColor);
+        }
+
+        private void calculatedescriptors(Mat image, UMat imageDescriptors, VectorOfKeyPoint imageKeyPoints)
+        {
+            using (UMat mImage = image.ToUMat(Emgu.CV.CvEnum.AccessType.Read))
+            {
+                SIFT sift = new SIFT();
+                sift.DetectAndCompute(mImage, null, imageKeyPoints, imageDescriptors, false);
+            }
+        }
+
+        private void videoCompute()
+        {
+            capture = new Emgu.CV.Capture(0);
+            timer1.Enabled=true;
+            viewer.ShowDialog();
+
+            time = 0;
+        }
+
+        private void viewer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (time == 10)
+            {
+                Mat frame = new Mat();
+                capture.Retrieve(frame, 0);
+                Mat grayVideo = new Mat();
+                CvInvoke.CvtColor(frame, grayVideo, ColorConversion.Bgr2Gray);
+                UMat videoDescriptors = new UMat();
+                VectorOfKeyPoint videoKeyPoints = new VectorOfKeyPoint();
+                calculatedescriptors(grayVideo, videoDescriptors, videoKeyPoints);
+                VectorOfVectorOfDMatch matches = new VectorOfVectorOfDMatch();
+
+                BFMatcher matcher = new BFMatcher(DistanceType.L2);
+                matcher.Add(originalImageDescriptors);
+                matcher.KnnMatch(videoDescriptors, matches, 2, null);
+                Mat mask = mask = new Mat(matches.Size, 1, DepthType.Cv8U, 1);
+                mask.SetTo(new MCvScalar(255));
+
+                Features2DToolbox.VoteForUniqueness(matches, 0.8, mask);
+                Mat homography = new Mat();
+                int nonZeroCount = CvInvoke.CountNonZero(mask);
+                if (nonZeroCount >= 4)
+                {
+                    nonZeroCount = Features2DToolbox.VoteForSizeAndOrientation(originalImageKeyPoints, videoKeyPoints,
+                       matches, mask, 1.5, 20);
+                    if (nonZeroCount >= 4)
+                        homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(originalImageKeyPoints, videoKeyPoints, matches, mask, 2);
+                }
+
+                Mat result = new Mat();
+                Features2DToolbox.DrawMatches(grayImage, originalImageKeyPoints, grayVideo, videoKeyPoints,
+                   matches, result, new MCvScalar(255, 255, 255), new MCvScalar(255, 255, 255), mask);
+                if (homography != null)
+                {
+                    //draw a rectangle along the projected model
+                    Rectangle rect = new Rectangle(Point.Empty, grayImage.Size);
+                    PointF[] pts = new PointF[]
+                    {
+                              new PointF(rect.Left, rect.Bottom),
+                              new PointF(rect.Right, rect.Bottom),
+                              new PointF(rect.Right, rect.Top),
+                              new PointF(rect.Left, rect.Top)
+                    };
+                    pts = CvInvoke.PerspectiveTransform(pts, homography);
+
+                    Point[] points = Array.ConvertAll<PointF, Point>(pts, Point.Round);
+                    using (VectorOfPoint vp = new VectorOfPoint(points))
+                    {
+                        CvInvoke.Polylines(result, vp, true, new MCvScalar(255, 0, 0, 255), 5);
+                    }
+                    viewer.Image = result;
+                    
+                }
+
+                
+                time = 0; 
+            }
+            else
+            {
+                time++;
+            }
+    }
+    }
+}
